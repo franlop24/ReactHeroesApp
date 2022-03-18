9866c77c9127251b282b63ef3c7d706e7e95ca51
Francisco Lopez <franlopbri@gmail.com>
2022-03-18 11:32:11 -0600

    Tarjetas con informacion del heroe

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,4 +1,5 @@
 import { getHeroesByPublisher } from "../../helpers/getHeroesByPublisher"
+import { HeroCard } from "./HeroCard";
 
 
 export const HeroList = ({ publisher }) => {
@@ -11,16 +12,15 @@ export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher)
     return (
-        <>
-            <ul>
+        <div className="row row-cols-1 row-cols-md-3 g-3">
                 {
                     heroes.map( hero => (
-                        <li key={ hero.id }>
-                            { hero.superhero }
-                        </li>
+                        <HeroCard 
+                            key={ hero.id } 
+                            { ...hero } 
+                            />
                     ))
                 }
-            </ul>
-        </>
+        </div>
     )
 }
